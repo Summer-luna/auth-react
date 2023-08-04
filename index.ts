24a12ea2036a6a34b64dff37bfd8847ad24053f0
Xinyue (Luna) <xinyuec57@gmail.com>
2023-08-04 17:22:07 -0400

    modules

---
+++ index.ts
@@ -0,0 +1,5 @@
+import { AuthGuard } from "./src/auth/auth.guard";
+import { UserId } from "./src/auth/user.decorator";
+import { Roles } from "./src/auth/roles.decorator";
+
+export { AuthGuard, UserId, Roles };
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { AuthGuard } from "./auth/auth.guard";
+import { UserId } from "./auth/user.decorator";
+import { Roles } from "./auth/roles.decorator";
+
+export { AuthGuard, UserId, Roles };
