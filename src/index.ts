d1765e29edd08d29a79abb6a408e1d70d6549222
Xinyue (Luna) <xinyuec57@gmail.com>
2023-08-07 15:56:11 -0400

    add user model

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import { AuthGuard } from "./auth/auth.guard";
 import { UserId } from "./auth/user.decorator";
 import { Roles } from "./auth/roles.decorator";
 import { JwtModule } from "./jwt/jwt.module";
+import { UserModel } from "./model/user.model";
 
-export { AuthGuard, UserId, Roles, JwtModule };
+export { AuthGuard, UserId, Roles, JwtModule, UserModel };
